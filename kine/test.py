5d42fc638c874973072890c9794f05cab8390930
Colin Summers <colin.x.summers@jpl.nasa.gov>
2017-07-31 20:28:16 -0700

    converging

--- kine/test.py
+++ kine/test.py
@@ -14,34 +14,56 @@ def get_adjoint(transform):
     res[:3, 3:] = matrix_rep.dot(rot)
     return res
 
-class TwoDKinematicChain(object):
+class InverseKinematics(object):
+
+    def __init__(self, kinematic_chain, target, alpha=0.1, eta=0.001):
+        self.kinematic_chain = kinematic_chain
+        self.target = target
+        self.alpha = alpha
+        self.eta = eta
+
+    def get_error(self):
+        err_mat = self.kinematic_chain.get_transform() \
+                       .dot(np.linalg.inv(self.target.get_transform())) \
+                       - np.eye(4)
+        err_vec = np.zeros(6)
+        err_vec[:3] = err_mat[:3, 3]
+        err_vec[3] = err_mat[2, 2] + err_mat[2, 1] + err_mat[1, 1]
+        err_vec[4] = err_mat[0, 0] + err_mat[0, 2] + err_mat[2, 2]
+        err_vec[5] = err_mat[1, 1] + err_mat[1, 0] + err_mat[0, 0]
+        return err_vec
+
+    def get_delta_theta(self):
+        return self.kinematic_chain.get_jacobian().transpose().dot(self.get_error())
+
+class KinematicChain(object):
 
     def __init__(self, initial_configuration):
         self.configuration = initial_configuration
         self.chain = []
         for link in self.configuration:
-            self.chain.append(TwoDKinematicLink(**link))
+            self.chain.append(KinematicLink(**link))
 
-    def compute_total_transform(self):
+    def get_transform(self):
         total_transform = np.eye(4)
         for link in self.chain:
             total_transform = total_transform.dot(link.get_transform())
         return total_transform
 
-    def compute_jacobian(self):
+    def get_jacobian(self):
         transform = self.chain[0].get_transform()
         jacobian = []
         for i in range(1, len(self.chain)):
             transform = transform.dot(self.chain[i].get_transform())
-            if self.chain[i - 1].screw is not None:
+            if self.chain[i].screw is not None:
                 # end of a link
-                screw = self.chain[i - 1].screw
+                screw = self.chain[i].screw
                 adj = get_adjoint(transform)
                 jacobian_i = adj.dot(screw)
                 jacobian.append(jacobian_i)
         return np.array(jacobian).transpose()
 
-class TwoDKinematicLink(object):
+class KinematicLink(object):
 
     def __init__(self, theta=0, x=0, y=0, scale=1, screw=None, name='KinematicLink'):
         self.transform = np.asarray([[np.cos(theta), -np.sin(theta), 0, x],
@@ -53,12 +75,11 @@ class TwoDKinematicLink(object):
         self.y = y
         self.scale = scale
         self.screw = np.array(screw) if screw != None else None
-        print self.screw
         self.name = name
         
     def set_theta(self, theta):
-        self.transform[:2, :2] = np.asarray([[np.cos(theta), -np.sin(theta)],
-                                    [np.sin(theta), np.cos(theta)]])
+        self.transform[:2, :2] = [[np.cos(theta), -np.sin(theta)],
+                                  [np.sin(theta), np.cos(theta)]]
         self.theta = theta
     
 
@@ -78,15 +99,28 @@ class TwoDKinematicLink(object):
         return self.transform
 
 joint_config = [{'name' : '0-0+'},
-                {'name' : '0+1-', 'theta' : np.pi / 4, 'screw' : [0, 0, 0, 0, 0, 1]},
+                {'name' : '0+1-', 'theta' : 0, 'screw' : [0, 0, 0, 0, 0, 1]},
+                {'name' : '1-1+', 'x' : 1},
+                {'name' : '1+2-', 'theta' : 0, 'screw' : [0, 0, 0, 0, 0, 1]}, 
+                {'name' : '2-2+', 'x' : 1}]
+target_config = [{'name' : '0-0+'},
+                {'name' : '0+1-', 'theta' : np.pi / 6, 'screw' : [0, 0, 0, 0, 0, 1]},
                 {'name' : '1-1+', 'x' : 1},
                 {'name' : '1+2-', 'theta' : np.pi / 4, 'screw' : [0, 0, 0, 0, 0, 1]}, 
                 {'name' : '2-2+', 'x' : 1}]
 #                {'name' : '2+3-', 'theta' : np.pi / 2, 'screw' : [0, 0, 0, 0, 0, 1]},
 #                {'name' : '3-3+', 'x' : 30}]
 
-chain = TwoDKinematicChain(joint_config)
-print chain.compute_jacobian()
-transform = chain.compute_total_transform()
-print transform.dot(np.asarray([0, 0, 0, 1]))
+chain = KinematicChain(joint_config)
+target = KinematicChain(target_config)
+print type(target)
 
+invk = InverseKinematics(chain, target, 0.1, 0.001)
+alpha = -0.1
+for i in range(0, 1000):
+    print chain.get_transform().dot(np.array([0,0,0,1]))
+    print np.linalg.norm((invk.get_error()))
+    delta_theta = invk.get_delta_theta()
+    chain.chain[1].set_theta(chain.chain[1].theta + alpha * delta_theta[0])
+    chain.chain[3].set_theta(chain.chain[3].theta + alpha * delta_theta[1])
+    
