678a8088aa47d36b48c7587f256af246a24f9e46
Mark Edmonds <mark@mjedmonds.com>
2017-08-29 11:33:50 -0700

    updated notes for setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_lock',
       version='0.0.1',
+      # Box2D must be installed from source
       install_requires=['gym', 'numpy', 'pyglet', 'Box2D', 'pygraphviz', 'pymdbptoolbox', 'transitions']
       )
