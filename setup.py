f6b8b685aff8e175734bc010036293f48e54e715
Mark Edmonds <mark@mjedmonds.com>
2018-01-13 21:07:51 -0800

    updated dependency for dqn agent saving

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='gym_lock',
       version='0.0.1',
       # Box2D must be installed from source
-      install_requires=['gym', 'numpy', 'pyglet', 'Box2D', 'pygraphviz', 'pymdptoolbox', 'transitions', 'shapely', 'jsonpickle', 'keras', 'tensorflow']
+      install_requires=['gym', 'numpy', 'pyglet', 'Box2D', 'pygraphviz', 'pymdptoolbox', 'transitions', 'shapely', 'jsonpickle', 'keras', 'tensorflow', 'h5py']
       )
