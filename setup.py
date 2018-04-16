2a1c15932cca5b60e5571c1ebf50060f559212c6
Mark Edmonds <mark@mjedmonds.com>
2018-04-16 16:07:31 -0700

    Removed Box2D from setup.py for Semaphore CI. Must be installed from source.

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='gym_lock',
       version='0.0.1',
-      # Box2D must be installed from source
-      install_requires=['gym', 'numpy', 'pyglet', 'Box2D', 'pygraphviz', 'pymdptoolbox', 'transitions', 'shapely', 'jsonpickle', 'keras', 'h5py', 'matplotlib']
+      # Box2D must be installed from source, and is not listed here
+      install_requires=['gym', 'numpy', 'pyglet', 'pygraphviz', 'pymdptoolbox', 'transitions', 'shapely', 'jsonpickle', 'keras', 'h5py', 'matplotlib']
       )
