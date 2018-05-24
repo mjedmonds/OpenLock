52447fa47bea6ff3c360fca1064bd504cb2b944c
ws <wllmshao@gmail.com>
2018-05-23 18:41:07 -0700

    added session_manager.py docstrings

--- session_manager.py
+++ session_manager.py
@@ -13,6 +13,14 @@ from matplotlib import pyplot as plt
 import tensorflow as tf
 
 class SessionManager():
+    """
+    Manage an environment and an agent in order to enforce a common routine across all scenarios and agents.
+
+    The class provides an interface for different sessions with different environments and agents
+    to operate the same way from a high level perspective. It has an environment and an agent
+    as class variables, which it coordinates using several methods, some to be used for all
+    scenarios and agents, and others for running trials with specific agents.
+    """
 
     env = None
     writer = None
@@ -20,6 +28,13 @@ class SessionManager():
     completed_trials = []
 
     def __init__(self, env, agent=None, params=None):
+        """
+        Set the instance's env, agent, and params. Initialize completed_trials to the empty list.
+
+        :param env: environment managed by this session (e.g. an object returned by gym.make())
+        :param agent: agent managed by this session (some Agent object)
+        :param params: dictionary of other parameters
+        """
         self.env = env
         self.params = params
         self.agent = agent
@@ -28,6 +43,19 @@ class SessionManager():
 
     # code to run before human and computer trials
     def run_trial_common_setup(self, scenario_name, action_limit, attempt_limit, specified_trial=None, multithreaded=False):
+        """
+        Set the env class variables and select a trial (specified if provided, otherwise a random trial from the scenario name).
+
+        This method should be called before running human and computer trials.
+        Returns the trial selected (string).
+
+        :param scenario_name: name of scenario (e.g. those defined in settings_trial.PARAMS)
+        :param action_limit: number of actions permitted
+        :param attempt_limit: number of attempts permitted
+        :param specified_trial: optional specified trial. If none, get_trial is used to select trial
+        :param multithreaded:
+        :return: the selected_trial as returned by get_trial or select_trial
+        """
         # setup trial
         self.env.attempt_count = 0
         self.env.attempt_limit = attempt_limit
@@ -58,6 +86,13 @@ class SessionManager():
 
     # code to run after both human and computer trials
     def run_trial_common_finish(self, trial_selected, test_trial):
+        """
+        Reset variables after finishing trial and call agent.finish_trial(). Add finished trial to completed_trials.
+
+        :param trial_selected: trial to add to completed_trials
+        :param test_trial: trial used for agent.finish_trial()
+        :return: Nothing
+        """
         # todo: detect whether or not all possible successful paths were uncovered
         self.agent.finish_trial(test_trial)
         self.completed_trials.append(copy.deepcopy(trial_selected))
@@ -66,6 +101,17 @@ class SessionManager():
 
     # code to run a human subject
     def run_trial_human(self, scenario_name, action_limit, attempt_limit, specified_trial=None, verify=False, test_trial=False):
+        """
+        Run trial for a human subject.
+
+        :param scenario_name: name of scenario (e.g. those defined in settings_trial.PARAMS)
+        :param action_limit: number of actions permitted
+        :param attempt_limit: number of attempts permitted
+        :param specified_trial: optional specified trial
+        :param verify: flag to indicate whether or not to call verify_fsm_matches_simulator()
+        :param test_trial: default: False
+        :return: Nothing
+        """
         self.env.human_agent = True
         trial_selected = self.run_trial_common_setup(scenario_name, action_limit, attempt_limit, specified_trial)
 
@@ -82,6 +128,20 @@ class SessionManager():
 
     # code to run a computer trial
     def run_trial_dqn(self, scenario_name, action_limit, attempt_limit, trial_count, iter_num, test_trial=False, specified_trial=None, fig=None, fig_update_rate=100):
+        """
+        Run a computer trial.
+
+        :param scenario_name:
+        :param action_limit:
+        :param attempt_limit:
+        :param trial_count:
+        :param iter_num:
+        :param test_trial:
+        :param specified_trial:
+        :param fig:
+        :param fig_update_rate:
+        :return:
+        """
         self.env.human_agent = False
         trial_selected = self.run_trial_common_setup(scenario_name, action_limit, attempt_limit, specified_trial)
 
@@ -155,6 +215,20 @@ class SessionManager():
 
     # code to run a computer trial ddqn priority memory replay
     def run_trial_ddqn_prority(self, scenario_name, action_limit, attempt_limit, trial_count, iter_num, test_trial=False, specified_trial=None, fig=None, fig_update_rate=100):
+        """
+        Run a computer trial (ddqn priority memory replay).
+
+        :param scenario_name:
+        :param action_limit:
+        :param attempt_limit:
+        :param trial_count:
+        :param iter_num:
+        :param test_trial:
+        :param specified_trial:
+        :param fig:
+        :param fig_update_rate:
+        :return:
+        """
         self.env.human_agent = False
         trial_selected = self.run_trial_common_setup(scenario_name, action_limit, attempt_limit, specified_trial)
 
@@ -230,6 +304,33 @@ class SessionManager():
     def run_trial_a3c(self, sess, global_episodes, number, testing_trial, params, coord,attempt_limit,
                       scenario_name, trial_count,is_test,a_size,MINI_BATCH,gamma,episode_rewards,episode_lengths,episode_mean_values,
                       summary_writer, name, saver, model_path, REWARD_FACTOR, fig=None):
+        """
+        Run a trial for a3c.
+
+        :param sess:
+        :param global_episodes:
+        :param number:
+        :param testing_trial:
+        :param params:
+        :param coord:
+        :param attempt_limit:
+        :param scenario_name:
+        :param trial_count:
+        :param is_test:
+        :param a_size:
+        :param MINI_BATCH:
+        :param gamma:
+        :param episode_rewards:
+        :param episode_lengths:
+        :param episode_mean_values:
+        :param summary_writer:
+        :param name:
+        :param saver:
+        :param model_path:
+        :param REWARD_FACTOR:
+        :param fig:
+        :return:
+        """
         self.env.human_agent = False
         episode_count = sess.run(global_episodes)
         increment = global_episodes.assign_add(1)
@@ -413,6 +514,18 @@ class SessionManager():
 
     # code to run a computer trial using q-tables (UNFINISHED)
     def run_trial_qtable(self, scenario_name, action_limit, attempt_limit, trial_count, iter_num, testing=False, specified_trial=None):
+        """
+        Run a computer trial using q-tables.
+
+        :param scenario_name:
+        :param action_limit:
+        :param attempt_limit:
+        :param trial_count:
+        :param iter_num:
+        :param testing:
+        :param specified_trial:
+        :return:
+        """
         self.env.human_agent = False
         trial_selected = self.run_trial_common_setup(scenario_name, action_limit, attempt_limit, specified_trial)
 
@@ -455,12 +568,25 @@ class SessionManager():
         self.agent.average_trial_rewards.append(trial_reward / attempt_limit)
 
     def update_scenario(self, scenario):
+        """
+        Set the environment's scenario to the specified scenario.
+
+        :param scenario: new scenario to use
+        :return: Nothing
+        """
         self.env.scenario = scenario
         self.env.solutions = scenario.solutions
         self.env.completed_solutions = []
         self.env.cur_action_seq = []
 
     def update_attempt(self, multithread=False):
+        """
+        Check if the action count has reached the action limit.
+        If so, increment the environment attempt_count and call the env.reset() method.
+
+        :param multithread: default: False
+        :return: boolean representing whether or not the environment was reset
+        """
         reset = False
         # above the allowed number of actions, need to increment the attempt count and reset the simulator
         if self.env.action_count >= self.env.action_limit:
@@ -496,6 +622,15 @@ class SessionManager():
         return reset
 
     def update_user(self, attempt_success, multithreaded=False):
+        """
+        Print update to the user.
+        Either all solutions have been found, there are solutions remaining, or the user has
+        reached the attempt limit and the trial is over without finding all solutions.
+
+        :param attempt_success:
+        :param multithreaded:
+        :return: two booleans, the first representing whether the trial is finished, the second representing whether the simulator should pause (for when the user opened the door).
+        """
         pause = False
         num_solutions_remaining = len(self.env.solutions) - len(self.env.completed_solutions)
         # continue or end trial
@@ -527,6 +662,12 @@ class SessionManager():
         return trial_finished, pause
 
     def finish_action(self, multithread=False):
+        """
+        Log an action and determine whether or not to reset by calling update_attempt.
+
+        :param multithread:
+        :return: env_reset: False or update_attempt()
+        """
         env_reset = False
         if not self.env.action_ack:
             if self.agent.logger.cur_trial.cur_attempt is None:
@@ -545,16 +686,46 @@ class SessionManager():
         return env_reset
 
     def set_action_limit(self, action_limit):
+        """
+        Set self.env.action_limit.
+
+        :param action_limit: new self.env.action_limit
+        :return: Nothing
+        """
         self.env.action_limit = action_limit
 
     def add_attempt(self):
+        """
+        Log the attempt and reset env.cur_action_seq.
+
+        :return: Nothing
+        """
         self.env.cur_action_seq = []
         self.agent.logger.cur_trial.add_attempt()
 
     def print_update(self, iter_num, trial_num, scenario_name, episode, episode_max, a_reward, t_reward, epsilon):
+        """
+        Print ID, iteration number, trial number, scenario, episode, attempt_reward, trial_reward, epsilon.
+
+        :param iter_num:
+        :param trial_num:
+        :param scenario_name:
+        :param episode:
+        :param episode_max:
+        :param a_reward:
+        :param t_reward:
+        :param epsilon:
+        :return: Nothing
+        """
         print("ID: {}, iter {}, trial {}, scenario {}, episode: {}/{}, attempt_reward {}, trial_reward {}, e: {:.2}".format(self.agent.subject_id, iter_num, trial_num, scenario_name, episode, episode_max, a_reward, t_reward, epsilon))
 
     def verify_fsm_matches_simulator(self, obs_space):
+        """
+        Ensure that the simulator data matches the FSM.
+
+        :param obs_space:
+        :return: obs_space
+        """
         if obs_space is None:
             obs_space = ObservationSpace(len(self.env.world_def.get_levers()))
         state, labels = obs_space.create_discrete_observation_from_simulator(self.env)
@@ -571,6 +742,11 @@ class SessionManager():
         return obs_space
 
     def dqn_trial_sanity_checks(self):
+        """
+        Used by dqn & ddqn trials to make sure attempt_seq and reward_seq are valid.
+
+        :return: Nothing
+        """
         try:
             if len(self.agent.trial_switch_points) > 0:
                 assert(len(self.agent.logger.cur_trial.attempt_seq) == len(self.agent.rewards) - self.agent.trial_switch_points[-1])
@@ -594,6 +770,14 @@ class SessionManager():
 
     @staticmethod
     def get_trial(name, completed_trials=None):
+        """
+        Apply specific rules for selecting random trials.
+        Namely, For CE4 & CC4, only selects from trials 7-11, otherwise only selects from trials 1-6.
+
+        :param name: Name of trial
+        :param completed_trials:
+        :return: trial and configs
+        """
         # select a random trial and add it to the scenario
         if name != 'CE4' and name != 'CC4':
             # trials 1-6 have 3 levers for CC3/CE3
