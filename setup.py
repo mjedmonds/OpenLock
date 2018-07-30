587fc3367a7464ad1126e6d450d9979d56065eb8
Mark Edmonds <mark@mjedmonds.com>
2018-07-30 09:20:58 -0500

    Updated setup.py to use setuptools and updated gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='OpenLock',
       version='1.0',
@@ -9,4 +12,5 @@ setup(name='OpenLock',
       author_email='mark@mjedmonds.com',
       url='https://github.com/mjedmonds/OpenLock',
       packages=['openlock', 'openlock.scenarios', 'openlock.envs', 'openlock.envs.world_defs'],
+      install_requires=required,
      )
